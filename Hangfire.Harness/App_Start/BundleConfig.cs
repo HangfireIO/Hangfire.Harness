1f07ce1ec0636d3cb742d3172a9e37751cd0f1f3
Sergey Odinokov <odinserj@hangfire.io>
2024-11-27 13:42:09 +0700

    Remove the bootstrap package

--- Hangfire.Harness/App_Start/BundleConfig.cs
+++ Hangfire.Harness/App_Start/BundleConfig.cs
@@ -10,11 +10,7 @@ namespace Hangfire.Harness
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
--- Hangfire.Harness/Scripts/_references.js
+++ Hangfire.Harness/Scripts/_references.js
@@ -1,4 +1,3 @@
 ﻿/// <autosync enabled="true" />
-/// <reference path="bootstrap.js" />
 /// <reference path="jquery-3.7.1.min.js" />
 /// <reference path="jquery-3.7.1.slim.min.js" />
